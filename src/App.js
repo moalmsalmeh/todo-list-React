166a02377e9ae58eeeb740857c01f90522a8d789
moalmsalmeh <mohammad.msalmeh@stud.h-da.de>
2026-02-02 07:28:23 +0100

    Refactor todo app and improve input handling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [todos,setTodos] = useState([]);
+   const[error,setError]= useState("");
+
+  const inputRef= useRef();
+ 
+  const handleAdTodo = () =>{
+    const text = inputRef.current.value;
+    const newItem={text, completed:false}
+    if(!text){
+      setError("Please add something!!!");
+      return;
+    }
+    setTodos([...todos,newItem]);
+     setError("");
+    inputRef.current.value="";
+  }
+  const handleItemDone =(index)=>{
+    const newTodos= [...todos];
+    newTodos[index].completed= !newTodos[index].completed;
+    setTodos(newTodos);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>To Do List</h2>
+     {error && <p className="error">{error}</p>}
+      <div className='to-do-container'>
+        <ul>
+        {todos.map((item, index) =>{
+          return <li className={item.completed ? "done" : ""} key={index} onClick={() => handleItemDone(index)}>{item.text}</li>
+        })}
+      </ul>
+      <input ref={inputRef} placeholder='Enter Item..'/>
+      <button onClick={handleAdTodo}>Add</button>
+      </div>
+      
     </div>
   );
 }
